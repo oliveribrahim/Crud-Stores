baf6b9d7a58f5df0b22b387e6317d4a3de12d3c0
oliveribrahim <oliveribrahim21@gmail.com>
2025-12-29 13:15:34 +0200

    sec commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<main>
+    <h1>oliver</h1>
+</main>
+
+</body>
+</html>
+
+</main>
+</body>
+
+</html>
